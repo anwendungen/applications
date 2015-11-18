4be9c0f43968b054f86e8201c28f502c595de3d7
anonymoussc <50c5ac69@opayq.com>
2015-11-18 18:13:33 +0700

    Add routes.

---
+++ singlepage/routes.js
@@ -0,0 +1,186 @@
+/**
+ * Created by anonymoussc on 18/11/15 17:31.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app.routes')
+        .config(function($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                .state('dashboard', {
+                    abstract: true,
+                    url     : '/dashboard',
+                    views   : {
+                        'layout@'               : {
+                            templateUrl: layout('dashboard.layout')
+                        },
+                        'header@dashboard'      : {
+                            templateUrl: view('dashboard.header')
+                        },
+                        'leftsection@dashboard' : {
+                            templateUrl: view('dashboard.leftsection')
+                        },
+                        'rightsection@dashboard': {
+                            templateUrl: view('dashboard.rightsection')
+                        },
+                        'footer@dashboard'      : {
+                            templateUrl: view('dashboard.footer')
+                        },
+                        'main@dashboard'        : {}
+                    }
+                })
+                .state('dashboard.home', {
+                    url  : '/',
+                    data : { pageName: 'Overview' },
+                    views: {
+                        'main@dashboard': {
+                            templateUrl: view('dashboard.home')
+                        }
+                    }
+                })
+                .state('app', {
+                    abstract: true,
+                    views   : {
+                        'layout@'         : {
+                            templateUrl: layout('app.layout')
+                        },
+                        'header@app'      : {
+                            templateUrl: view('app.header')
+                        },
+                        'leftsection@app' : {
+                            templateUrl: view('app.leftsection')
+                        },
+                        'rightsection@app': {
+                            templateUrl: view('app.rightsection')
+                        },
+                        'footer@app'      : {
+                            templateUrl: view('app.footer')
+                        },
+                        'main@app'        : {}
+                    }
+                })
+                .state('app.home', {
+                    url  : '/',
+                    data : { pageName: 'Homepage' },
+                    views: {
+                        'main@app': {
+                            templateUrl: view('app.home')
+                        }
+                    }
+                })
+                .state('jwtauth', {
+                    abstract: true,
+                    url     : '/auth',
+                    views   : {
+                        'layout@'             : {
+                            templateUrl: layout('jwt_auth.layout')
+                        },
+                        'header@jwtauth'      : {
+                            templateUrl: view('jwt_auth.header')
+                        },
+                        'leftsection@jwtauth' : {
+                            templateUrl: view('jwt_auth.leftsection')
+                        },
+                        'rightsection@jwtauth': {
+                            templateUrl: view('jwt_auth.rightsection')
+                        },
+                        'footer@jwtauth'      : {
+                            templateUrl: view('jwt_auth.footer')
+                        },
+                        'main@jwtauth'        : {}
+                    }
+                })
+                .state('jwtauth.signin', {
+                    url  : '/sign-in',
+                    data : { pageName: 'Sign-in' },
+                    views: {
+                        'main@jwtauth': {
+                            templateUrl: view('jwt_auth.signin')
+                        }
+                    }
+                })
+                .state('staticpage', {
+                    abstract: true,
+                    url     : '/page',
+                    views   : {
+                        'layout@'                : {
+                            templateUrl: layout('staticpage.layout')
+                        },
+                        'header@staticpage'      : {
+                            templateUrl: view('staticpage.header')
+                        },
+                        'leftsection@staticpage' : {
+                            templateUrl: view('staticpage.leftsection')
+                        },
+                        'rightsection@staticpage': {
+                            templateUrl: view('staticpage.rightsection')
+                        },
+                        'footer@staticpage'      : {
+                            templateUrl: view('staticpage.footer')
+                        },
+                        'main@staticpage'        : {}
+                    }
+                })
+                .state('staticpage.landing', {
+                    url  : '/',
+                    data : { pageName: 'Overview' },
+                    views: {
+                        'main@staticpage': {
+                            templateUrl: view('staticpage.landing')
+                        }
+                    }
+                });
+
+            function view(viewName) {
+                if (viewName !== "") {
+                    return './views/app/' + appName(viewName) + '/' + fileDir(viewName) + '/' + fileName(viewName) + '.html';
+                } else {
+                    return './views/app/app/home/home.html';
+                }
+            }
+
+            function layout(viewName) {
+                if (viewName !== "") {
+                    return './views/themes/' + appName(viewName) + '/' + fileDir(viewName) + '/' + fileName(viewName) + '.html';
+                } else {
+                    return './views/app/app/home/home.html';
+                }
+
+            }
+
+            function appName(v) {
+                if (v.split(".")[0]) {
+                    return v.split(".")[0];
+                } else {
+                    return 'app';
+                }
+            }
+
+            function fileDir(v) {
+                if (v.split(".")[1]) {
+                    return v.split(".")[1];
+                } else if (!v.split(".")[0]) {
+                    return v;
+                } else {
+                    return 'home';
+                }
+            }
+
+            function fileName(v) {
+                if (v.split(".")[2]) {
+                    return v.split(".")[2];
+                } else if (!v.split(".")[2]) {
+                    if (v.split(".")[1]) {
+                        return v.split(".")[1];
+                    }
+                } else {
+                    return 'home';
+                }
+            }
+        });
+})();
---
+++ singlepage/routes.run.js
@@ -0,0 +1,23 @@
+/**
+ * Created by anonymoussc on 18/11/15 17:31.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app.routes')
+        .run(function($rootScope, $mdSidenav) {
+            $rootScope.$on("$stateChangeStart", function(event, toState) {
+
+                if (toState.data && toState.data.pageName) {
+                    $rootScope.current_page = toState.data.pageName;
+                }
+
+            });
+            $rootScope.$on("$viewContentLoaded", function(event, toState) {
+                window.Prism.highlightAll();
+            });
+        });
+
+})();
