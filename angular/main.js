182566a651b6e8ef1412833d0ee9627605b81cd9
anonymoussc <50c5ac69@opayq.com>
2015-11-21 16:34:42 +0700

    Update main.js , main.*.js

--- angular/main.js
+++ angular/main.js
@@ -5,33 +5,6 @@
 (function() {
     'use strict';
 
-    angular.module('app',
-        [
-            'app.config',
-            'app.controllers',
-            'app.directives',
-            'app.factory',
-            'app.filters',
-            'app.services',
-            'app.routes',
-            'app.component'
-        ]);
-
-    angular.module('app.config', []);
-    angular.module('app.controllers', [
-        'ui.router',
-        'ngMaterial',
-        'ngStorage',
-        'restangular',
-        'ngMdIcons',
-        'angular-loading-bar',
-        'ngMessages',
-        'vAccordion'
-    ]);
-    angular.module('app.directives', []);
-    angular.module('app.factory', []);
-    angular.module('app.filters', []);
-    angular.module('app.services', []);
-    angular.module('app.routes', []);
+    angular.module('mainApp', ['app', 'components']);
 
 })();
