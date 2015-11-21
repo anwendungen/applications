a154a19258513858273f12ab98c2db4dbb23d440
anonymoussc <50c5ac69@opayq.com>
2015-11-22 04:18:48 +0700

    Add index

---
+++ src/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html ng-app="applications">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/prism.css') !!}">
+    <link href='//fonts.googleapis.com/css?family=Roboto:500,400' rel='stylesheet' type='text/css'>
+    <title ng-bind="title || 'Application'"></title>
+    <!--[if lte IE 10]>
+    <script type="text/javascript">document.location.href = '/unsupported-browser'</script>
+    <![endif]-->
+</head>
+<body>
+
+<div ui-view="layout"></div>
+
+<script src="{!! asset('js/vendor.js') !!}"></script>
+<script src="{!! asset('js/app.js') !!}"></script>
+<script src="{!! asset('js/prism.js') !!}"></script>
+
+{{--livereload--}}
+@if ( Config::get('app.debug') )
+    <script type="text/javascript">
+        document.write('<script src="'+ location.protocol + '//' + (location.host || 'localhost') +':35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+    </script>
+@endif
+</body>
+</html>
+
+
+{{--Created by anonymoussc on 22/11/15 4:12.--}}
