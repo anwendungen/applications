cdf2a7979cc8a0bef4ed988efb2fd4df08bd7c6b
anonymoussc <50c5ac69@opayq.com>
2015-11-24 18:12:06 +0700

    Update appService

--- angular/services/appService.js
+++ angular/services/appService.js
@@ -5,14 +5,14 @@
      * @ngdoc service
      * @name app
      * @description
-     * _Please update the description and dependencies._
+     * _Update the description and dependencies._
      *
      * */
-    angular.module('app.services').service('appService', function() {
+    angular.module('app.services').service('appService', [function() {
 
         this.testMethod = function() {
 
         };
 
-    });
+    }]);
 })();
