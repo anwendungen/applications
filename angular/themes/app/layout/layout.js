5b3cd513f8c234d7eebdedc807bf9bd3f25b4d23
anonymoussc <50c5ac69@opayq.com>
2015-11-24 18:19:08 +0700

    Update layout.js

--- angular/themes/app/layout/layout.js
+++ angular/themes/app/layout/layout.js
@@ -2,52 +2,59 @@
  * Created by anonymoussc on 23/11/15 23:46.
  */
 
-'use strict';
+(function() {
+    'use strict';
 
-angular
-    .module('app.controllers')
-    .controller('LayoutCtrl', function($scope, $timeout, $mdSidenav, $log) {
-        $scope.toggleLeft = buildDelayedToggler('left');
-        $scope.toggleRight = buildToggler('right');
-        $scope.isOpenRight = function(){
-            return $mdSidenav('right').isOpen();
-        };
-        /**
-         * Supplies a function that will continue to operate until the
-         * time is up.
-         */
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = $scope,
-                    args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
+    angular
+        .module('app.controllers')
+        .controller('LayoutCtrl', function($scope, $timeout, $mdSidenav, $log) {
+            $scope.toggleLeft  = buildDelayedToggler('left');
+            $scope.toggleRight = buildToggler('right');
+            $scope.isOpenRight = function() {
+                return $mdSidenav('right').isOpen();
             };
-        }
-        /**
-         * Build handler to open/close a SideNav; when animation finishes
-         * report completion in console
-         */
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function() {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }, 200);
-        }
-        function buildToggler(navID) {
-            return function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function() {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            };
-        }
-    });
+
+            /**
+             * Supplies a function that will continue to operate until the
+             * time is up.
+             */
+            function debounce(func, wait, context) {
+                var timer;
+                return function debounced() {
+                    var context = $scope,
+                        args    = Array.prototype.slice.call(arguments);
+                    $timeout.cancel(timer);
+                    timer       = $timeout(function() {
+                        timer = undefined;
+                        func.apply(context, args);
+                    }, wait || 10);
+                };
+            }
+
+            /**
+             * Build handler to open/close a SideNav; when animation finishes
+             * report completion in console
+             */
+            function buildDelayedToggler(navID) {
+                return debounce(function() {
+                    $mdSidenav(navID)
+                        .toggle()
+                        .then(function() {
+                            $log.debug("toggle " + navID + " is done");
+                        });
+                }, 200);
+            }
+
+            function buildToggler(navID) {
+                return function() {
+                    $mdSidenav(navID)
+                        .toggle()
+                        .then(function() {
+                            $log.debug("toggle " + navID + " is done");
+                        });
+                };
+            }
+        });
+
+})();
+
