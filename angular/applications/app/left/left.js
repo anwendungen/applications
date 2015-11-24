d346b1db5e5b2d448c31fc972b55b18183651be1
anonymoussc <50c5ac69@opayq.com>
2015-11-25 03:25:29 +0700

    Update layout & partial

--- angular/applications/app/left/left.js
+++ angular/applications/app/left/left.js
@@ -9,7 +9,7 @@
         $scope.close = function() {
             $mdSidenav('left').close()
                 .then(function() {
-                    $log.debug("close LEFT is done");
+                    // $log.debug("close LEFT is done");
                 });
         };
     }]);
--- angular/applications/app/right/right.js
+++ angular/applications/app/right/right.js
@@ -9,7 +9,7 @@
         $scope.close = function() {
             $mdSidenav('right').close()
                 .then(function() {
-                    $log.debug("close RIGHT is done");
+                    // $log.debug("close RIGHT is done");
                 });
         };
     }]);
