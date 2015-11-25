05661f930c944e09ff99281d7c65c2e5c81f8eeb
anonymoussc <50c5ac69@opayq.com>
2015-11-26 04:02:39 +0700

    Updates header

--- angular/applications/dashboard/footer/footer.js
+++ angular/applications/dashboard/footer/footer.js
@@ -4,8 +4,8 @@
 
 (function() {
     'use strict';
-    
+
     angular.module('app.controllers').controller('DashboardFooterCtrl', ['$scope', function($scope) {
-        
+
     }]);
 })();
--- angular/applications/dashboard/header/header.js
+++ angular/applications/dashboard/header/header.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 24/11/15 3:29.
+ * Created by anonymoussc on 26/11/15 4:00.
  */
 
 (function() {
     'use strict';
-    
-    angular.module('app.controllers').controller('HeaderCtrl', ['$scope', function($scope) {
-        
+
+    angular.module('app.controllers').controller('DashboardHeaderCtrl', ['$scope', function($scope) {
+
     }]);
 })();
