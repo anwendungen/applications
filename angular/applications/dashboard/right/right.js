5bc5f1ace5e38133a7ffc7044cb6638a2dcb4c13
anonymoussc <50c5ac69@opayq.com>
2015-11-26 05:40:25 +0700

    Updates dashboard right section

--- angular/applications/dashboard/right/right.js
+++ angular/applications/dashboard/right/right.js
@@ -1,16 +1,15 @@
 /**
- * Created by anonymoussc on 24/11/15 3:29.
+ * Created by anonymoussc on 26/11/15 5:38.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('RightCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
+    angular.module('app.controllers').controller('DashboardRightCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function() {
-            $mdSidenav('right').close()
-                .then(function() {
-                    // $log.debug("close RIGHT is done");
-                });
+            $mdSidenav('right').close().then(function() {
+                // $log.debug("close RIGHT is done");
+            });
         };
     }]);
 })();
