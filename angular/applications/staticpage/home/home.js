9db13519c591a3e2047d7fc9b42fe0bcf0b60c38
anonymoussc <50c5ac69@opayq.com>
2015-11-26 18:12:44 +0700

    Update staticpage home page

--- angular/applications/staticpage/home/home.js
+++ angular/applications/staticpage/home/home.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 26/11/15 4:04.
+ * Created by anonymoussc on 26/11/15 18:09.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('DashboardHomeCtrl', ['$scope', function($scope) {
+    angular.module('app.controllers').controller('StaticpageHomeCtrl', ['$scope', function($scope) {
 
     }]);
 })();
