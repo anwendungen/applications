758a4f8217c7bb435ecd20201b0ab6c4db12673f
anonymoussc <50c5ac69@opayq.com>
2015-11-26 21:52:47 +0700

    Update simple layout header

--- angular/applications/app/header/header.js
+++ angular/applications/app/header/header.js
@@ -4,8 +4,13 @@
 
 (function() {
     'use strict';
-    
-    angular.module('app.controllers').controller('HeaderCtrl', ['$scope', function($scope) {
-        
+
+    angular.module('app.controllers').controller('HeaderCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $scope.$watch(function() {
+            return $rootScope.current_page;
+        }, function(newPage) {
+            $scope.current_page = newPage || 'Page Name';
+        });
+
     }]);
 })();
--- angular/applications/dashboard/header/header.js
+++ angular/applications/dashboard/header/header.js
@@ -5,7 +5,12 @@
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('DashboardHeaderCtrl', ['$scope', function($scope) {
+    angular.module('app.controllers').controller('DashboardHeaderCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $scope.$watch(function() {
+            return $rootScope.current_page;
+        }, function(newPage) {
+            $scope.current_page = newPage || 'Page Name';
+        });
 
     }]);
 })();
