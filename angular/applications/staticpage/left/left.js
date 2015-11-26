fa714fa39226b388d1192f865f77ed07b1d60e0c
anonymoussc <50c5ac69@opayq.com>
2015-11-26 18:15:00 +0700

    Update staticpage left section

--- angular/applications/staticpage/left/left.js
+++ angular/applications/staticpage/left/left.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 26/11/15 4:24.
+ * Created by anonymoussc on 26/11/15 18:13.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('DashboardLeftCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
+    angular.module('app.controllers').controller('StaticpageLeftCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function() {
             $mdSidenav('left').close().then(function() {
                 // $log.debug("close LEFT is done");
