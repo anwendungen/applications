9c0720edf76e3bc81566e51fbdc4fc7b8eae5dd4
anonymoussc <50c5ac69@opayq.com>
2015-12-05 21:48:10 +0700

    Updates routes.php

--- src/routes.php
+++ src/routes.php
@@ -10,15 +10,3 @@ Route::get('/', function () {
 Route::get('/unsupported-browser', function () {
     return view('applications::unsupported_browser');
 });
-
-$api = app('Dingo\Api\Routing\Router');
-
-$api->version('v1', function ($api) {
-    $api->post('authenticate', 'App\Http\Controllers\AuthenticateController@authenticate');
-});
-
-// Protected with JWT
-$api->version('v1', ['middleware' => 'api.auth'], function ($api) {
-    $api->get('authenticate', 'App\Http\Controllers\AuthenticateController@index');
-    $api->get('authenticate/user', 'App\Http\Controllers\AuthenticateController@getAuthenticatedUser');
-});
