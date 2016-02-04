703ac5b54e257d2e594677741e3e4d3e6ece39c6
anonymoussc <50c5ac69@opayq.com>
2016-02-04 08:53:07 +0700

    Modified applications.module

--- ngmodules/applications.module.js
+++ ngmodules/applications.module.js
@@ -17,7 +17,20 @@
 
             /* service components */
             'dashboard',
-            'seneschal'
+            'seneschal',
+            'client'
         ]);
+
+})();
+
+/**
+ * client.module.js
+ * Created by anonymous on 09/12/15 13:12.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('client', []);
 
 })();
