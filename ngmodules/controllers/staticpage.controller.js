7cebd22589a1347a7c6540478880cd6c2b01c8ef
anonymoussc <50c5ac69@opayq.com>
2016-02-04 08:56:55 +0700

    New staticpage.controller

---
+++ ngmodules/controllers/staticpage.controller.js
@@ -0,0 +1,29 @@
+/**
+ * staticpage.controller.js
+ * Created by anonymous on 10/12/15 7:35.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('client')
+        .controller('StaticpageController', StaticpageController);
+
+    StaticpageController.$inject = [];
+
+    /* @ngInject */
+    function StaticpageController() {
+        var vm   = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
