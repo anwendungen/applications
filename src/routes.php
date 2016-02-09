8ed5a87560f1b2690d15f6c5933079dffdff4fe8
anonymoussc <50c5ac69@opayq.com>
2016-02-09 18:53:34 +0700

    Modified routes

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,15 @@
 <?php
 /**
+ * routes.php
  * Created by anonymoussc on 22/11/15 5:03.
  */
 
-Route::get('/', function () {
-    return view('applications::index');
-});
+Route::get('/', [
+    'as' => 'home',
+    function () {
+        return view('applications::index');
+    },
+]);
 
 Route::get('/unsupported-browser', function () {
     return view('applications::unsupported_browser');
