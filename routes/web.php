91065e5bb6382e321d1fe35510a60a47cbda6c9e
anonymoussc <50c5ac69@opayq.com>
2016-11-19 13:57:18 +0700

    Modified ./routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,13 @@
  * web.php
  * Created by anonymoussc on 11/19/2016 1:36 AM.
  */
+
+Route::get('/', [
+    'as'   => 'home',
+    'uses' => 'Anwendungen\Applications\Controllers\ApplicationsController@appStart',
+]);
+
+Route::get('/unsupported-browser', [
+    'as'   => 'app.unsupported',
+    'uses' => 'Anwendungen\Applications\Controllers\ApplicationsController@appUnsupported',
+]);
